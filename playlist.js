5fd0442a47d6b3fecf37278cfd0ff5f7e4089cde
schimnesthai <157383701+schimnesthai@users.noreply.github.com>
2024-03-20 12:59:08 -0300

    Create playlist.js

---
+++ playlist.js
@@ -0,0 +1,11 @@
+    var sites = [
+        'http://www.google.com',
+        'http://www.stackoverflow.com',
+        'http://www.example.com',
+        'http://www.youtube.com'
+    ];
+
+    function randomSite() {
+        var i = parseInt(Math.random() * sites.length);
+        location.href = sites[i];
+    }
